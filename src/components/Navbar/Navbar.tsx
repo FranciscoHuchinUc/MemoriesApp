44d8fc5eef42f50202ac56026bf9f138668b2501
Francisco Huchin <huchin.uc.francisco@hotmail.com>
2022-09-21 21:04:07 -0500

    REMOVE: Padding Y

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,11 +4,11 @@ import Plus from '@/assets/icons/Plus.svg'
 import Image from '@/assets/icons/Image.svg'
 import { Item } from './Item'
 
-export interface NavbarInterface {}
+export interface NavbarInterface { }
 
 const Navbar: React.FC<NavbarInterface> = () => {
 	return (
-		<nav className='w-full h-[60px] p-[10px] absolute bottom-0 left-0'>
+		<nav className='w-full h-[60px] px-[10px]  absolute bottom-0 left-0'>
 			<ul className='flex flex-row justify-between items-center px-[30px] py-1 rounded-md bg-secondary/70 backdrop-blur-sm'>
 				<Item to='/' icon={Heart} text='Home' />
 				<Item to='/add' icon={Plus} />
