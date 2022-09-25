2b3803949597a9a5e3a09b99842b4d322579d655
Francisco Huchin <huchin.uc.francisco@hotmail.com>
2022-09-24 23:54:30 -0500

    ADD: Añadido la funcionalidad de subir archivos a Storage de Firebase

--- src/lib/firestore/config.ts
+++ src/lib/firestore/config.ts
@@ -1,17 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCHCnyJggMehhaC94YsR0gI8FHvRVjAmsk",
-  authDomain: "memoriesapp-f4aa3.firebaseapp.com",
-  projectId: "memoriesapp-f4aa3",
-  storageBucket: "memoriesapp-f4aa3.appspot.com",
-  messagingSenderId: "783327740184",
-  appId: "1:783327740184:web:831471fb99a56a75f65e6d"
-};
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: 'memoriesapp-f4aa3.firebaseapp.com',
+	projectId: 'memoriesapp-f4aa3',
+	storageBucket: 'memoriesapp-f4aa3.appspot.com',
+	messagingSenderId: '783327740184',
+	appId: '1:783327740184:web:831471fb99a56a75f65e6d',
+}
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
+export const storage = getStorage(app)
--- src/lib/firestore/db.ts
+++ src/lib/firestore/db.ts
@@ -1,6 +1,8 @@
-import {db} from './config'
-import {collection, addDoc, getDocs, query, orderBy, limit} from 'firebase/firestore'
-import {Memorie} from '@/interfaces/interfaces'
+import { db, storage } from './config'
+import { collection, addDoc, getDocs, query, orderBy, limit } from 'firebase/firestore'
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
+import { Memorie } from '@/interfaces/interfaces'
+import { v4 } from 'uuid'
 
 const memoriesRef = collection(db, 'memories')
 
@@ -29,4 +31,11 @@ export const lastMemorie = async () => {
 		}
 	})
 	return last
+}
+
+export const uploadImage = async (file: any) => {
+	const storageRef = ref(storage, `post/${v4()}`)
+	await uploadBytes(storageRef, file)
+	const url = await getDownloadURL(storageRef)
+	return url
 }
