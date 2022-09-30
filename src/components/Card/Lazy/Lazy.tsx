d057de45b4eb6ea119459a5620e83b7aa8ccba6f
Francisco Huchin <huchin.uc.francisco@hotmail.com>
2022-09-30 11:21:42 -0500

    FIXED: Rediseño y Responsive del Lazy

--- src/components/Card/Lazy/Lazy.tsx
+++ src/components/Card/Lazy/Lazy.tsx
@@ -6,15 +6,13 @@ export interface LazyInterface { }
 const Lazy: React.FC<LazyInterface> = () => {
 	return (
 		<Fragment>
-			<section className="bg-white border rounded-md w-full h-[410px] overflow-hidden shadow-xl">
+			<section className="bg-white border rounded-md w-full h-[410px] overflow-hidden sm:w-[470px]">
 				<div className="animate-pulse">
 					<header className="bg-gray-200 flex items-center w-full h-[300px] overflow-hidden"></header>
-					<footer className="py-2 px-1">
-						<div className="space-y-3">
-							<div className="h-2 bg-gray-200 rounded"></div>
-							<div className="h-2 bg-gray-200 rounded"></div>
-							<div className="h-2 bg-gray-200 rounded"></div>
-						</div>
+					<footer className="flex flex-col justify-center gap-1 p-2  h-[110px]">
+						<div className="h-2 bg-gray-200 rounded"></div>
+						<div className="h-2 bg-gray-200 rounded mt-1"></div>
+						<div className="h-2 w-20 bg-gray-200 rounded mt-4"></div>
 					</footer>
 				</div>
 			</section>
