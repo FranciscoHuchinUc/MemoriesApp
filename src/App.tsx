cae693b7aada973b9256a240943f0df92a1e8e8c
Francisco Huchin <huchin.uc.francisco@hotmail.com>
2022-09-29 23:52:53 -0500

    UPDATE: Route Â'/' Replace to ¿'Home'

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { Navbar } from './components'
 import { AddMemorie, Home, Memories } from './pages'
 
-function App() {
+function App () {
 
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/add" element={<AddMemorie />} />
         <Route path='/memories' element={<Memories />} />
+        <Route path="/" element={<Navigate to="/home" replace />} />
       </Routes>
     </>
   )
