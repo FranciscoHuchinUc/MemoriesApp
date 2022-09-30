d3fd9054e812b6e2882422a315b7c7dbc1f54297
Francisco Huchin <huchin.uc.francisco@hotmail.com>
2022-09-29 23:46:27 -0500

    ADD: Configuración de Iconly

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import * as path from 'path'
 import react from '@vitejs/plugin-react'
-import {defineConfig} from 'vite'
-import {VitePWA} from 'vite-plugin-pwa'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import manifest from './manifest.json'
 
@@ -27,5 +27,8 @@ export default defineConfig({
 				replacement: path.resolve(__dirname, 'src'),
 			},
 		],
+	},
+	define: {
+		'process.env': {},
 	},
 })
